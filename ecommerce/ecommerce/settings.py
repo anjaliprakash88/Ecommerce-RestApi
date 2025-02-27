d816f1aee41e7b44399c523d2f199862b691e4e5
athulraj909 <athulrajn1999@gmail.com>
2025-02-27 13:23:29 +0530

    third and fourth

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Dental_Clinic',
+        'NAME': 'ecommerce',
         'USER': 'postgres',
-        'PASSWORD':'soft_pkd',
+        'PASSWORD':'1234',
         'HOST': 'localhost'
 
     }
@@ -127,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
--- ecommerce/main/views.py
+++ ecommerce/main/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from . import serializers
+from rest_framework import generics,permissions
+from . import models
 
 # Create your views here.
+
+class VendorList(generics.ListCreateAPIView):
+    queryset = models.Vendor.objects.all()
+    serializer_class = serializers.VendorSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+
+
+class VendorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Vendor.objects.all()
+    serializer_class = serializers.VendorDetailSerializer
+    # permission_classes = [permissions.IsAuthenticated]

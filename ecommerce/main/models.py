756f1934d22b049d9e2d0b90e06bb92e335bb30c
athulraj909 <athulrajn1999@gmail.com>
2025-02-27 16:16:57 +0530

    Resolved merge conflicts

--- ecommerce/main/models.py
+++ ecommerce/main/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=200)
     detail = models.TextField(null=True)
-    price = models.FloatField
+    price = models.FloatField()
 
     def __str__(self):
         return self.title

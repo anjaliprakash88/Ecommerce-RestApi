3322c331ef9fe2e0bd601e56690a4550fc925d17
anjaliprakash88 <anjaliprakash455@gmail.com>
2025-03-17 10:46:37 +0530

    Home page:Latest Product Section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+  <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-success">
         <div className="container">
           <a className="navbar-brand" href="#">Python Market Place</a>
@@ -23,6 +24,137 @@ function App() {
           </div>
         </div>
       </nav>
+
+
+      <main className="mt-4">
+      <div className="container">
+      <h3 className="mb-4">Latest products<a href="#" className="float-end btn btn-dark">View all products<i class="fa-solid fa-arrow-right-long"></i></a></h3>
+      <div className="row">
+      {/* Product box */}
+      <div className="col-12 col-md-3 mb-4">
+      <div className="card">
+          <img src={logo} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h4 className="card-title">Product title</h4>
+            <h5 className="card-title text-muted">Price: Rs 500</h5>
+       </div>
+       <div className="card-footer">
+       <button title="add to cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
+       <button title="add to wishlist" className="btn btn-danger btn-sm ms-2"><i class="fa fa-heart"></i></button>
+       </div>
+      </div>
+      </div>
+      {/* Product box */}
+      {/* Product box */}
+      <div className="col-12 col-md-3 mb-4">
+      <div className="card">
+          <img src={logo} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h4 className="card-title">Product title</h4>
+            <h5 className="card-title text-muted">Price: Rs 500</h5>
+       </div>
+       <div className="card-footer">
+       <button title="add to cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
+       <button title="add to wishlist" className="btn btn-danger btn-sm ms-2"><i class="fa fa-heart"></i></button>
+       </div>
+      </div>
+      </div>
+      {/* Product box */}
+      {/* Product box */}
+      <div className="col-12 col-md-3 mb-4">
+      <div className="card">
+          <img src={logo} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h4 className="card-title">Product title</h4>
+            <h5 className="card-title text-muted">Price: Rs 500</h5>
+       </div>
+       <div className="card-footer">
+       <button title="add to cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
+       <button title="add to wishlist" className="btn btn-danger btn-sm ms-2"><i class="fa fa-heart"></i></button>
+       </div>
+      </div>
+      </div>
+      {/* Product box */}
+      {/* Product box */}
+      <div className="col-12 col-md-3 mb-4">
+      <div className="card">
+          <img src={logo} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h4 className="card-title">Product title</h4>
+            <h5 className="card-title text-muted">Price: Rs 500</h5>
+       </div>
+       <div className="card-footer">
+       <button title="add to cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
+       <button title="add to wishlist" className="btn btn-danger btn-sm ms-2"><i class="fa fa-heart"></i></button>
+       </div>
+      </div>
+      </div>
+      {/* Product box */}
+      {/* Product box */}
+      <div className="col-12 col-md-3 mb-4">
+      <div className="card">
+          <img src={logo} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h4 className="card-title">Product title</h4>
+            <h5 className="card-title text-muted">Price: Rs 500</h5>
+       </div>
+       <div className="card-footer">
+       <button title="add to cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
+       <button title="add to wishlist" className="btn btn-danger btn-sm ms-2"><i class="fa fa-heart"></i></button>
+       </div>
+      </div>
+      </div>
+      {/* Product box */}
+      {/* Product box */}
+      <div className="col-12 col-md-3 mb-4">
+      <div className="card">
+          <img src={logo} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h4 className="card-title">Product title</h4>
+            <h5 className="card-title text-muted">Price: Rs 500</h5>
+       </div>
+       <div className="card-footer">
+       <button title="add to cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
+       <button title="add to wishlist" className="btn btn-danger btn-sm ms-2"><i class="fa fa-heart"></i></button>
+       </div>
+      </div>
+      </div>
+      {/* Product box */}
+      {/* Product box */}
+      <div className="col-12 col-md-3 mb-4">
+      <div className="card">
+          <img src={logo} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h4 className="card-title">Product title</h4>
+            <h5 className="card-title text-muted">Price: Rs 500</h5>
+       </div>
+       <div className="card-footer">
+       <button title="add to cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
+       <button title="add to wishlist" className="btn btn-danger btn-sm ms-2"><i class="fa fa-heart"></i></button>
+       </div>
+      </div>
+      </div>
+      {/* Product box */}
+      {/* Product box */}
+      <div className="col-12 col-md-3 mb-4">
+      <div className="card">
+          <img src={logo} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h4 className="card-title">Product title</h4>
+            <h5 className="card-title text-muted">Price: Rs 500</h5>
+       </div>
+       <div className="card-footer">
+       <button title="add to cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
+       <button title="add to wishlist" className="btn btn-danger btn-sm ms-2"><i class="fa fa-heart"></i></button>
+       </div>
+      </div>
+      </div>
+      {/* Product box */}
+
+      </div>
+      </div>
+      </main>
+      </>
       
   );
 }
